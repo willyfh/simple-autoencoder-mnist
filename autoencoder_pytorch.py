c7e86752ee6881d514567991db997d39cff80496
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2019-06-06 19:18:48 +0000

    Update autoencoder_pytorch.py

--- autoencoder_pytorch.py
+++ autoencoder_pytorch.py
@@ -1,12 +1,6 @@
 """
 __author__  = Willy Fitra Hendria
 
-Simple autoencoder with 2 hidden layers on each encoder and decoder.
-The first hidden-layer will have 256 units, and the second 128 units.
-Sigmoid used as the activation function.
-The initial weight matrices and biases are randomly sampled from normal distribution.
-Training by minimizing the average mean squared error.
-Reconstructed image will be plotted after finishing the iterations.
 """
 
 import os
